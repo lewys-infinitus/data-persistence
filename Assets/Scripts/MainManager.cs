4456cc6614c2d1f9a6b02073f1c40529dc15aad7
lewys-infinitus <161516810+lewys-infinitus@users.noreply.github.com>
2024-06-24 22:22:11 +0100

    edited scripts

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -12,20 +12,22 @@ public class MainManager : MonoBehaviour
 
     public Text ScoreText;
     public GameObject GameOverText;
-    
+
+   // public int HighScore; // Corrected variable name
+  //  public string PlayerName; // Corrected variable name
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
     // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -72,5 +74,26 @@ public class MainManager : MonoBehaviour
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+
+        //if (m_Points > HighScore)
+       // {
+       //     HighScore = m_Points;
+       // }
+    }
+
+    public static MainManager Instance;
+
+    public int EnterPlayerName;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 }
--- Assets/Scripts/UI/MenuUIHandler.cs
+++ Assets/Scripts/UI/MenuUIHandler.cs
@@ -1,18 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuUIHandler : MonoBehaviour
 {
+   // public InputField nameInputField;
+   // public Text HighScoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-
+       // DisplayHighScore();
     }
 
     // Update is called once per frame
@@ -45,4 +51,44 @@ public class MenuUIHandler : MonoBehaviour
             Application.Quit();
 #endif
     }
+
+    public void EnterPlayerName()
+    {
+        int enterPlayerName = MainManager.Instance.EnterPlayerName;
+    }
+
+   // public void DisplayHighScore()
+   // {
+     //   int highScore = MainManager.Instance.HighScore;
+     //   HighScoreText.text = "High Score: " + highScore;
+        // Display the high score along with the player's name if available
+//if (!string.IsNullOrEmpty(MainManager.Instance.PlayerName))
+       // {
+       //     HighScoreText.text += " by " + MainManager.Instance.PlayerName;
+       // }
+   // }
+
+   // public void SubmitName()
+   // {
+        //string playerName = nameInputField.text; // Corrected variable name
+
+        //if (!string.IsNullOrEmpty(playerName))
+      //  {
+          //  MainManager.Instance.PlayerName = playerName;
+          //  DisplayHighScore();
+      //  }
+       // else
+      //  {
+          //  Debug.LogWarning("Player name is empty. Please enter a name.");
+      //  }
+   // }
+
+    // public void UpdateHighScore(int newScore)
+  //  {
+        //if (newScore > MainManager.Instance.HighScore)
+     //   {
+           // MainManager.Instance.HighScore = newScore;
+          //  DisplayHighScore();
+       // }
+   // }
 }
